8587db99acff935f094ab21dd30c16a4274e0587
sapkyy <sapkyy@gmail.com>
2025-01-03 16:09:04 +0000

    add docker lxc

---
+++ lxc_105_docker.tf
@@ -0,0 +1,24 @@
+resource "proxmox_lxc" "docker" {
+  target_node  = "pve"
+  hostname     = "docker"
+  vmid         = "105"
+  memory       = "512"
+  cores        = "1"
+  ostemplate   = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  password     = var.LXC_PASS
+  unprivileged = true
+  start        = true
+  onboot       = true
+  nameserver   = "192.168.31.1"
+  searchdomain = "sapkyy.home"
+  rootfs {
+    storage = "local-zfs"
+    size    = "2G"
+  }
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+  ssh_public_keys = var.SSH_PUB_KEY
+}

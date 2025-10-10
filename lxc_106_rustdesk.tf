dbfa7f83cae2c8acb107166bd78425eb8b44b79d
sapkyy <sapkyy@gmail.com>
2025-10-10 20:55:28 +0400

    add rustdesk lxc

---
+++ lxc_106_rustdesk.tf
@@ -0,0 +1,24 @@
+resource "proxmox_lxc" "rustdesk" {
+  target_node  = "pve"
+  hostname     = "rustdesk"
+  vmid         = "106"
+  memory       = "512"
+  cores        = "1"
+  ostemplate   = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
+  password     = var.LXC_PASS
+  unprivileged = true
+  start        = true
+  onboot       = true
+  nameserver   = "192.168.31.1"
+  searchdomain = "sapkyy.home"
+  rootfs {
+    storage = "local-zfs"
+    size    = "4G"
+  }
+  network {
+    name   = "eth0"
+    bridge = "vmbr0"
+    ip     = "dhcp"
+  }
+  ssh_public_keys = var.SSH_PUB_KEY
+}

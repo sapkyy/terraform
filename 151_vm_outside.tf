c9cdd3158a9e470d7bfd773a55493d27bff1e528
sapkyy <sapkyy@gmail.com>
2026-01-13 19:09:52 +0400

    add outside

--- 151_vm_outside.tf
+++ 151_vm_outside.tf
@@ -1,27 +1,33 @@
-resource "proxmox_vm_qemu" "proxy" {
-  name        = "proxy"
+resource "proxmox_vm_qemu" "outside" {
+  name        = "outside"
   target_node = "pve"
   vmid        = 151
   memory      = 1024
-  cores       = 2
-  sockets     = 1
-  onboot      = true
+  start_at_node_boot = true
   agent       = 1
   scsihw      = "virtio-scsi-pci"
-  cpu         = "host"
   kvm         = true
 
   clone      = "ubuntu24-template"
   full_clone = true
 
+  cpu {
+    cores   = 2
+    sockets = 1
+    type    = "host"
+  }
+
   disk {
+    slot     = "scsi0"
     size     = "8G"
-    type     = "scsi"
+    type     = "disk"
     storage  = "local-zfs"
-    iothread = 1
+    iothread = false
+    format   = "raw"
   }
 
   network {
+    id     = 0
     model  = "virtio"
     bridge = "vmbr0"
   }
@@ -32,4 +38,10 @@ resource "proxmox_vm_qemu" "proxy" {
   ipconfig0  = "ip=dhcp"
 
   boot = "order=scsi0"
+
+  startup_shutdown {
+    shutdown_timeout = -1
+    startup_delay    = -1
+    order            = -1
+  }
 }
--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url  = "https://pve.sapkyy.home:8006/api2/json"
+  pm_api_url  = "https://pve.sapkyy.home/api2/json"
   pm_user     = var.PM_USER
   pm_password = var.PM_PASS
 }
